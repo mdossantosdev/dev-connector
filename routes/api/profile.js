239988d2fc744f2192a5dfbbf517e213168e9fdc
Marc Dos Santos <mdossantosdev@gmail.com>
2019-05-23 15:35:06 +0200

    Add delete education route to profile.js

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -305,4 +305,33 @@ router.put(
   }
 );
 
+/*
+  @route  DELETE api/profile/education/:edu_id
+  @desc   Delete education from profile
+  @access Private
+*/
+router.delete('/education/:edu_id', auth, async (req, res) => {
+  try {
+    const profile = await Profile.findOne({ user: req.user.id });
+
+    // Get remove index
+    const removeIndex = profile.education
+      .map((item) => item.id)
+      .indexOf(req.params.edu_id);
+
+    if (removeIndex === -1) {
+      return res.status(400).json({ msg: 'Education not found' });
+    }
+
+    profile.education.splice(removeIndex, 1);
+
+    await profile.save();
+
+    res.json(profile);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server error');
+  }
+});
+
 module.exports = router;
